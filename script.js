bbd51f6ea75c2b916d14d37699863c7244729ae7
ausclaw <163063096+ausclaw@users.noreply.github.com>
2025-02-19 22:55:24 -0500

    Added script for piano

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+//Variables for Keyboard Buttons
+
+const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
+const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j'];
+
+const whiteKeys = document.querySelectorAll('.key.white');
+const blackKeys = document.querySelectorAll('.key.black');
+
+
 //All keys
 
 const keys = document.querySelectorAll('.key');
@@ -9,9 +18,32 @@ keys.forEach((key) => {
   key.addEventListener('click', () => playNote(key));
 });
 
+document.addEventListener('keydown', (e) => {
+    // Get the key that triggered the event
+    const key = e.key;
+    const whiteKeyIndex = WHITE_KEYS.indexOf(key);
+    const blackKeyIndex = BLACK_KEYS.indexOf(key);
+  
+      // If the key is being held down, don't play the note again
+  if (e.repeat) {
+    return;
+  }
+    if (whiteKeyIndex > -1) {
+      playNote(whiteKeys[whiteKeyIndex]);
+    }
+  
+    if (blackKeyIndex > -1) {
+      playNote(blackKeys[blackKeyIndex]);
+    }
+  });
+
 // Handlers
 function playNote(key) {
     const noteAudio = document.getElementById(key.dataset.note);
     noteAudio.currentTime = 0;
     noteAudio.play();
+    key.classList.add('active');
+    noteAudio.addEventListener('ended', () => {
+        key.classList.remove('active');
+      });
 }
